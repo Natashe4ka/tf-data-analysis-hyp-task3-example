0d4e7a4056330ef2cf30d04362e0a4f45a4f9300
Natashe4ka <90340759+Natashe4ka@users.noreply.github.com>
2023-04-09 19:04:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import mannwhitneyu
+import statsmodels
+from statsmodels.stats.weightstats import ztest as ztest
+
+chat_id = 965404933  # Ваш chat ID, не меняйте название переменной
 
 
-chat_id = 965404933 # Ваш chat ID, не меняйте название переменной
-
-def solution(x, y) -> bool:
+def solution(x) -> bool:
     alpha = 0.06
-    res = mannwhitneyu(x, y, alternative='greater', method='asymptotic')
-    if res.pvalue <= alpha:
+    a,b = ztest(x, value=500, alternative='smaller')
+    if b <= alpha:
         return True
     else:
         return False
