adb94e1320430a9b96347e8ed1dd240929c3653f
Natashe4ka <90340759+Natashe4ka@users.noreply.github.com>
2023-04-09 17:54:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 965404933 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x, y) -> bool:
+    alpha = 0.06
+    res = mannwhitneyu(x, y, alternative='greater', method='asymptotic')
+    if res.pvalue <= alpha:
+        return True
+    else:
+        return False
